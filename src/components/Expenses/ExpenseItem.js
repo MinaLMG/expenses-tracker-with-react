c75a82f66b834562f0aa03235d10e29720496e0b
minaLMG <beandro6@gmail.com>
2022-03-03 18:57:47 +0200

    added a button with an event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,11 @@
+import { click } from "@testing-library/user-event/dist/click";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
+  function clickHandler() {
+    console.log("clicked!!!!!");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -9,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>change title</button>
     </Card>
   );
 }
