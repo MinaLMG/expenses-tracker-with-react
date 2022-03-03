0966580ac5721b0f91afbd7748bd20da9b5d4942
minaLMG <beandro6@gmail.com>
2022-03-03 16:28:38 +0200

    added the initial structure for ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,12 @@
 function ExpenseItem() {
-  return <h2> Expense Item! </h2>;
+  return (
+    <div>
+      <div>date</div>
+      <div>
+        <h2>title</h2>
+        <div>amount</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
