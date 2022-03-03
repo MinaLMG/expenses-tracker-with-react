15fb4f9bfab89c9014962f02f93d8efbfc1a6256
minaLMG <beandro6@gmail.com>
2022-03-03 17:01:03 +0200

    added dynamic values for date,title and amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "car insurace";
+  const expenseAmount = 249.79;
   return (
     <div className="expense-item">
-      <div>date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>title</h2>
-        <div className="expense-item__price">amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
